0f3a6a0bb1e8ab30e4b14dfa85a0db31515029bb
Zachary Roitman <zar7@cornell.edu>
2023-10-16 18:46:46 -0400

    Start graph interface
    - example.ml is an example of creating a widget
    - Added graph.ml

---
+++ src/graph.ml
@@ -0,0 +1,12 @@
+open Bogue
+module W = Widget
+module L = Layout
+
+let width = 800
+let height = 600
+
+(* let build_axis d r = let area = W.sdl_area ~w:width ~h:height () in Draw.line
+   ~color:Draw.(opaque black) ~x0:0 ~y0:0 ~x1:width ~y1:height
+
+   let board = (build_axis dim (W.box ())) |> L.resident |> Bogue.of_layout let
+   () = Bogue.run board *)
