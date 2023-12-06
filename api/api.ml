453b0c779f6eb6aa3105177f8e2d81864e62e4f8
zsj5 <zsj5@cornell.edu>
2023-12-06 16:40:39 -0500

    .
    added exceptions

--- api/api.ml
+++ api/api.ml
@@ -3,18 +3,14 @@ open Cohttp
 open Cohttp_lwt_unix
 open Yojson.Basic
 open Unix
+open Exceptions
 
-let ping_id (ticker : string) () =
-  let uri =
-    Uri.of_string
-      ("https://apistocks.p.rapidapi.com/intraday?symbol=" ^ ticker
-     ^ "&interval=5min&maxreturn=100")
-  in
-
+let ping (ticker : string) (uri : Uri.t) () = 
   let headers =
     Header.init ()
     |> fun h ->
-    Header.add h "X-RapidAPI-Key" "5d3445235emshfc2077d35576f7ep154ce0jsnc22c8c609a52"
+    Header.add h "X-RapidAPI-Key" 
+    "5d3445235emshfc2077d35576f7ep154ce0jsnc22c8c609a52"
     |> fun h ->
     Header.add h "X-RapidAPI-Host" "apistocks.p.rapidapi.com"
   in
@@ -26,6 +22,18 @@ let ping_id (ticker : string) () =
      >>= fun body_str ->
      let json = Yojson.Basic.from_string body_str in
      Lwt.return json)
+
+let ping_id (ticker : string) () =
+  let uri =
+    Uri.of_string
+      ("https://apistocks.p.rapidapi.com/intraday?symbol=" ^ ticker
+     ^ "&interval=5min&maxreturn=100")
+  in (ping ticker uri ())
+
+let ping_day ticker (start_day : string) (end_day : string) () =
+  let uri = Uri.of_string ("https://apistocks.p.rapidapi.com/daily?symbol=" ^ 
+  ticker ^ "&dateStart=" ^ start_day ^ "&dateEnd=" ^ end_day) in
+  (ping ticker uri ())
 
 let print_json ticker () =
   let json_result = (ping_id ticker ()) in
@@ -41,7 +49,7 @@ let print_json_results ticker () =
         | _ -> []
       in
       Yojson.Basic.pretty_to_string (`List results)
-  | _ -> invalid_arg "Invalid JSON format"
+  | _ -> raise InvalidJSONFormat
 
 let print_last_results ticker () =
   let json_data = ping_id ticker () in
@@ -55,48 +63,55 @@ let print_last_results ticker () =
       (match List.rev results with
       | (`Assoc result_fields)::_ ->
           (Yojson.Basic.to_string (`Assoc result_fields))
-      | _ -> invalid_arg "No results found")
-  | _ -> invalid_arg "Invalid JSON format"
+      | _ -> raise NoResultsFound)
+  | _ -> raise InvalidJSONFormat
 
 let get_date_current ticker json_data =
   match json_data with
-  | `Assoc fields ->
+  | `Assoc fields -> (
       let results =
         match List.assoc "Results" fields with
         | `List results -> results
         | _ -> []
       in
-      (match List.rev results with
-      | (`Assoc result_fields)::_ ->
-          (match List.assoc "Date" result_fields with
+      match List.rev results with
+      | `Assoc result_fields :: _ -> (
+          match List.assoc "Date" result_fields with
           | `String date -> date
-          | _ -> invalid_arg "Date not found")
-      | _ -> invalid_arg "No results found")
-  | _ -> invalid_arg "Invalid JSON format"
+          | _ -> raise NoDateFound)
+      | _ -> raise NoResultsFound)
+  | _ -> raise InvalidJSONFormat
 
 let get_price_current ticker json_data =
   match json_data with
-  | `Assoc fields ->
+  | `Assoc fields -> (
       let results =
         match List.assoc "Results" fields with
         | `List results -> results
         | _ -> []
       in
-      (match List.rev results with
-      | (`Assoc result_fields)::_ ->
-          (match List.assoc "Close" result_fields with
+      match List.rev results with
+      | `Assoc result_fields :: _ -> (
+          match List.assoc "Close" result_fields with
           | `Float close_price -> close_price
-          | _ -> invalid_arg "Closing price not found")
-      | _ -> invalid_arg "No results found")
-  | _ -> invalid_arg "Invalid JSON format"
+          | _ -> raise NoClosingPriceFound)
+      | _ -> raise NoResultsFound)
+  | _ -> raise InvalidJSONFormat
+  
 
-let get ticker = 
+let get_id ticker = 
   let json_data = ping_id ticker () in
     (get_price_current ticker json_data, get_date_current ticker json_data)
 
-let get_price ticker = fst (get ticker)
+let get_price ticker = fst (get_id ticker)
 
 let local_time = Unix.localtime (Unix.time())
+
+let get_yr_m_d t =
+  let year = Printf.sprintf "%04d" (t.tm_year + 1900) in
+  let month = Printf.sprintf "%02d" (t.tm_mon + 1) in
+  let day = Printf.sprintf "%02d" t.tm_mday in
+  year ^ "-" ^ month ^ "-" ^ day
 
 let get_time =
   let get_hr_min =
@@ -110,16 +125,13 @@ let get_time =
     let min = Printf.sprintf "%02d" rounded_min in
     hr ^ ":" ^ min
   in
-  let t = local_time in
-  let year = Printf.sprintf "%04d" (t.tm_year + 1900) in
-  let month = Printf.sprintf "%02d" (t.tm_mon + 1) in
-  let day = Printf.sprintf "%02d" t.tm_mday in
-  year ^ "-" ^ month ^ "-" ^ day ^ " " ^ get_hr_min
+  (get_yr_m_d (local_time) )^ " " ^ get_hr_min
 
 let unix_time_of_string datetime_str =
   let split_datetime datetime_str =
     match String.split_on_char ' ' datetime_str with
-    | [date; time] -> Some (String.split_on_char '-' date, String.split_on_char ':' time)
+    | [date; time] -> 
+      Some (String.split_on_char '-' date, String.split_on_char ':' time)
     | _ -> None
   in
   match split_datetime datetime_str with
