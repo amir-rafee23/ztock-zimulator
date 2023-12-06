e77fb0585e5e7a012f8a2c39e4339004ee7b1255
zsj5 <zsj5@cornell.edu>
2023-12-05 20:07:58 -0500

    update api.ml
    added time and some other additional functions:
    added ability to hide functions from client view if desired

--- api/api.ml
+++ api/api.ml
@@ -45,7 +45,7 @@ let print_json_results ticker () =
       print_endline results_string
   | _ -> print_endline "Invalid JSON format"
 
-let get_closing_price ticker date_time =
+let print_last_results ticker () =
   let json_data = ping_id ticker () in
   match json_data with
   | `Assoc fields ->
@@ -54,43 +54,89 @@ let get_closing_price ticker date_time =
         | `List results -> results
         | _ -> []
       in
-      let matching_result =
-        List.find_opt
-          (function
-            | `Assoc result_fields ->
-                let date_value =
-                  List.assoc "Date" result_fields |> function
-                  | `String date -> date
-                  | _ -> ""
-                in
-                date_value = date_time
-            | _ -> false)
-          results
+      (match List.rev results with
+      | (`Assoc result_fields)::_ ->
+          print_endline (Yojson.Basic.to_string (`Assoc result_fields))
+      | _ -> invalid_arg "No results found")
+  | _ -> invalid_arg "Invalid JSON format"
+
+let get_date_current ticker json_data =
+  match json_data with
+  | `Assoc fields ->
+      let results =
+        match List.assoc "Results" fields with
+        | `List results -> results
+        | _ -> []
       in
-      (match matching_result with
-      | Some (`Assoc result_fields) ->
+      (match List.rev results with
+      | (`Assoc result_fields)::_ ->
+          (match List.assoc "Date" result_fields with
+          | `String date -> date
+          | _ -> invalid_arg "Date not found")
+      | _ -> invalid_arg "No results found")
+  | _ -> invalid_arg "Invalid JSON format"
+
+let get_price_current ticker json_data =
+  match json_data with
+  | `Assoc fields ->
+      let results =
+        match List.assoc "Results" fields with
+        | `List results -> results
+        | _ -> []
+      in
+      (match List.rev results with
+      | (`Assoc result_fields)::_ ->
           (match List.assoc "Close" result_fields with
           | `Float close_price -> close_price
-          | _ -> failwith "Closing price not found")
-      | _ -> failwith "Date and time not found")
-  | _ -> failwith "Invalid JSON format"
+          | _ -> invalid_arg "Closing price not found")
+      | _ -> invalid_arg "No results found")
+  | _ -> invalid_arg "Invalid JSON format"
+
+let get ticker = 
+  let json_data = ping_id ticker () in
+    (get_price_current ticker json_data, get_date_current ticker json_data)
+
+let get_price ticker = fst (get ticker)
 
 let local_time = Unix.localtime (Unix.time())
 
-let get_hr_min =
-  let round_by_five n = (n + 2) / 5 * 5 in
-  let t = local_time in
-  let hr = Printf.sprintf "%02d" t.tm_hour in
-  let rounded_min =
-    if t.tm_min > 10 then round_by_five t.tm_min - 20
-    else round_by_five t.tm_min + 40
-  in
-  let min = Printf.sprintf "%02d" rounded_min in
-  hr ^ ":" ^ min
-
 let get_time =
+  let get_hr_min =
+    let round_by_five n = (n + 2) / 5 * 5 in
+    let t = local_time in
+    let hr = Printf.sprintf "%02d" t.tm_hour in
+    let rounded_min =
+      if t.tm_min > 10 then round_by_five t.tm_min - 20
+      else round_by_five t.tm_min + 40
+    in
+    let min = Printf.sprintf "%02d" rounded_min in
+    hr ^ ":" ^ min
+  in
   let t = local_time in
   let year = Printf.sprintf "%04d" (t.tm_year + 1900) in
   let month = Printf.sprintf "%02d" (t.tm_mon + 1) in
   let day = Printf.sprintf "%02d" t.tm_mday in
   year ^ "-" ^ month ^ "-" ^ day ^ " " ^ get_hr_min
+
+let unix_time_of_string datetime_str =
+  let split_datetime datetime_str =
+    match String.split_on_char ' ' datetime_str with
+    | [date; time] -> Some (String.split_on_char '-' date, String.split_on_char ':' time)
+    | _ -> None
+  in
+  match split_datetime datetime_str with
+  | Some (date_parts, time_parts) ->
+    (match date_parts, time_parts with
+    | [year; month; day], [hour; minute] ->
+      let tm = {tm_year = int_of_string year - 1900; 
+                tm_mon = int_of_string month - 1; tm_mday = int_of_string day;
+                tm_hour = int_of_string hour; tm_min = int_of_string minute; 
+                tm_sec = 0; tm_wday = 0; tm_yday = 0; tm_isdst = false} in
+      Some (Unix.mktime tm)
+    | _ -> None)
+  | None -> None
+
+let unix_time datetime_str =
+  match unix_time_of_string datetime_str with
+    Some (f, _) -> f
+    | None -> failwith "Incorrect date time entered"
