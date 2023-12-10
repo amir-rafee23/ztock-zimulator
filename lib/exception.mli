e28f9d004f3e4ded4e779d3f4bbe4181e833a442
Zachary Roitman <zar7@cornell.edu>
2023-12-09 22:09:37 -0500

    Add exceptions to .mli

--- lib/exception.mli
+++ lib/exception.mli
@@ -1,2 +1,9 @@
 (** [Exception] defines custom exceptions that are thrown by [Api] and
     [Portfolio] functions to be caught by [Tui] functions*)
+
+exception NoResultsFound
+exception InvalidJSONFormat
+exception NoDateFound
+exception NoClosingPriceFound
+exception ExceededQuantity
+exception TickerNotHeld
