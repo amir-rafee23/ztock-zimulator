7e11f5907defa75e184cdd0dfeae14bb2452afb6
mb2563 <mb2563@cornell.edu>
2023-12-09 19:48:46 -0500

    Edit spec to fix [make doc]
    - Spec for [display_portfolio_filesys] edited.

--- lib/portfolio.mli
+++ lib/portfolio.mli
@@ -58,19 +58,20 @@ module type PortfolioType = sig
       hour:min:sec format).*)
 
   val display_portfolio_filesys : t -> string list list
-  (** [display_portfolio_filesys portfolio] is a list containing exactly all 
-  the data stored in the underlying concrete value of [portfolio], which is, 
-  for each stock, the ticker name, quantity currently held, initial buy date, 
-  buy batches and sell batches. Each inner list is a row, with each row itself 
-  being a list of column entries.
-      
-      Example output: 
+  (** [display_portfolio_filesys portfolio] is a list containing exactly all the
+      data stored in the underlying concrete value of [portfolio], which is, for
+      each stock, the ticker name, quantity currently held, initial buy date,
+      buy batches and sell batches. Each inner list is a row, with each row
+      itself being a list of column entries.
 
-    [[
+      Example output:
+
+      [[
     ["ticker"; "quantity"; "initial buy date"; "buy batches"; "sell batches"];
-    ["AAPL"; "66"; "32423.2"; "[{pb_1, qb_1, db_1}"; ... ;"{pb_m, qb_m, db_m}]"; 
-    "[{ps_1, qs_1, ds_1}; ... ;{ps_n, qs_n, ds_n}"]
-    ]]. *)
+    ["AAPL"; "66"; "32423.2"; "[{pb_1, qb_1, db_1}; ... ;{pb_m, qb_m, db_m}]"; 
+    "[{ps_1, qs_1, ds_1}; ... ;{ps_n, qs_n, ds_n}]"]
+    ]]
+      .*)
 end
 
 module String_map : Map.S with type key = string
